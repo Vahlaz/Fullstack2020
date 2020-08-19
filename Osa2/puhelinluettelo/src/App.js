22d2e08d3d8078235d8fbd655cffff6b353bf3bf
Vahlaz <raty.atte@gmail.com>
2020-08-19 22:09:25 +0300

    Puhelinluettelo

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react'
+
+const Content = ({ persons }) => {
+  const map1 = persons.map(parts =>( 
+  <li key = {parts.name}>
+    {parts.name + ' ' + parts.number}
+  </li>))
+  return (
+    <div>
+        {map1}
+    </div>
+  )
+}
+
+const Filter = ({persons, setNewSearch}) => {
+  const HandleSearchChange = (event) =>{
+    console.log(event.target.value)
+    setNewSearch(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+  const handleSubmit= (event) => {
+    event.preventDefault()
+  }
+
+  return ( 
+    <form onSubmit = {handleSubmit}> 
+    <div>
+      search: 
+       <input 
+      onChange={
+        HandleSearchChange}/>
+    </div>
+  </form>
+  )
+}
+
+const NewPersonForm = ({persons, setPersons,newName, setNewName, newNumber, setNewNumber}) =>{
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked')
+
+    const newPersonObject = {
+      name: newName,
+      number: newNumber
+    }
+    const found = persons.findIndex(element => element.name === newName)
+    console.log(found)
+  
+    if (found >= 0 )  {
+      window.alert(`${newName} is already added to phonebook`)
+    }
+
+     else if (found === -1){
+    setPersons(persons.concat(newPersonObject))
+    setNewName('')
+    setNewNumber('')
+    console.log(persons)
+  
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+return(
+  <form onSubmit={addName}>
+  <div> name: <input 
+    value = {newName} 
+    onChange={handleNameChange}/> </div>
+    
+    <div> Number: <input 
+      value = {newNumber}
+      onChange = {handleNumberChange} /> </div>
+   
+    <div>
+    <button type="submit">add</button>
+  </div>
+</form>
+)
+
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState([])
+
+useEffect(()=> {
+  setNewSearch(persons)
+},[persons])
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter persons = {persons} setNewSearch = {setNewSearch}/>
+      <h2>Add a new</h2>
+      <NewPersonForm persons = {persons} setPersons={setPersons} newName = {newName} setNewName={setNewName} newNumber = {newNumber} setNewNumber = {setNewNumber}  />
+      <h2>Numbers</h2>
+    <Content  persons = {
+            newSearch}/>
+    </div>
+  )
+}
+
+export default App
