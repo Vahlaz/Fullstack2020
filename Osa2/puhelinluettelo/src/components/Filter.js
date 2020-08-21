15f4fb00f38c4d793bf0ab397d4057d21e3aa9a7
Vahlaz <raty.atte@gmail.com>
2020-08-21 10:51:41 +0300

    Update Filter.js

--- Osa2/puhelinluettelo/src/components/Filter.js
+++ Osa2/puhelinluettelo/src/components/Filter.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 const Filter = ({persons, setNewSearch}) => {
   const HandleSearchChange = (event) =>{
-    console.log(event.target.value)
     setNewSearch(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
   }
   const handleSubmit= (event) => {
