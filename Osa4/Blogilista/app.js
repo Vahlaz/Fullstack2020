c4b1e0fffdba31bf1eab1c2de3c58587b7ddc7bf
Vahlaz <raty.atte@gmail.com>
2020-10-07 10:40:26 +0300

    osa4

--- Osa4/Blogilista/app.js
+++ Osa4/Blogilista/app.js
@@ -5,12 +5,14 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/userRouter')
 app.use(cors())
 app.use(express.json())
 
 app.use('/api', blogsRouter)
-
-
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
