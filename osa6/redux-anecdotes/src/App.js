6af95a60bdff54204f1cd94a53b3bab2bf18907f
Vahlaz <raty.atte@gmail.com>
2020-11-02 12:19:19 +0200

    osa6

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,38 +1,43 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import anecdoteReducer from './reducers/anecdoteReducer'
+import { vote, new_anecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state)
+  anecdotes.sort((a,b)=> b- a)
+	const dispatch = useDispatch()
 
-  const vote = async (id, VOTE) => {
-    console.log('vote', id)
-    dispatch(anecdoteReducer(id))
-
+	const handleVote = (id) => {
+		console.log('vote', id)
+		dispatch(vote(id))
+  }
+  
+  const handleSubmit =(event) => {
+    event.preventDefault()
+    dispatch(new_anecdote(event.target.anecdote.value))
   }
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={()=>vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+      <form onSubmit = {handleSubmit} >
+				<div>
+					<input type='text' name='anecdote' />
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	)
 }
 
 export default App
