84d517f93fb5f77105be9a4eb98b9a8f21176d6a
Vahlaz <raty.atte@gmail.com>
2020-11-05 19:58:00 +0200

    osa6

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -7,6 +7,8 @@ const Notification = () => {
 		padding: 10,
 		borderWidth: 1,
 	}
+	
+
 	const message = useSelector((state) => state.notification)
 	if (message) {
 		return <div style={style}> {message} </div>
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,29 +1,29 @@
 export const notification = (notification, time) => {
-	return async (dispatch) => {
-		dispatch({
-			type: 'NOTIFICATION',
-			data: notification,
-		})
-		
-		setTimeout(
-			() =>
-				dispatch({
-					type: 'NOTIFICATION',
-					data: '',
-				}),
-			time
-		)
-	}
+  return async (dispatch) => {
+    dispatch({
+      type: "NOTIFICATION",
+      data: notification,
+    })
+
+    setTimeout(
+      () =>
+        dispatch({
+          type: "NOTIFICATION",
+          data: "",
+        }),
+      time
+    )
+  }
 }
 
-const reducer = (state = '', action) => {
-	switch (action.type) {
-		case 'NOTIFICATION':
-			const changedState = action.data
-			return changedState
-		default:
-			return state
-	}
+const reducer = (state = "", action) => {
+  switch (action.type) {
+    case "NOTIFICATION":
+      const changedState = action.data
+      return changedState
+    default:
+      return state
+  }
 }
 
 export default reducer
