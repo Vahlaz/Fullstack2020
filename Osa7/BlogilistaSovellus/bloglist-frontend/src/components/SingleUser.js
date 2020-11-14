8ec747d4b4484f68006b31ef485221f03165e4c7
Vahlaz <raty.atte@gmail.com>
2020-11-14 12:36:24 +0200

    Update SingleUser.js

--- Osa7/BlogilistaSovellus/bloglist-frontend/src/components/SingleUser.js
+++ Osa7/BlogilistaSovellus/bloglist-frontend/src/components/SingleUser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useRouteMatch } from 'react-router-dom'
-
+import {Table} from 'react-bootstrap'
 const SingleUser = ({ users }) => {
   const match = useRouteMatch('/users/:id')
   if (users) {
@@ -8,13 +8,20 @@ const SingleUser = ({ users }) => {
     return (
       <>
         <h2> {userById.name} </h2>
-        <h3>added blogs</h3>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Added Blogs</th>
+            </tr>
+          </thead>
+          <tbody>
         {userById.blogs.map((blog) => (
-          <li key={blog.id}>
-            {' '}
+          <tr key={blog.id}>
             <Link to={`/blog/${blog.id}`}>{blog.title} </Link>{' '}
-          </li>
+          </tr>
         ))}
+        </tbody>
+        </Table>
       </>
     )
   }
